a56929aa22ac44dce658271334e492079cfba67f
Fílip Anselmo <filip.anselmo@jointecnologia.com.br>
2022-07-22 16:51:29 -0400

    Atualizando menu

--- src/components/ModalMapComponent.vue
+++ src/components/ModalMapComponent.vue
@@ -1,24 +1,19 @@
 <template>
   <div class="text-center">
-    <v-menu v-model="menu" :close-on-content-click="false" :nudge-width="200" offset-x>
-      <template v-slot:activator="{ on, attrs }">
+  <!-- 
+    <v-menu v-model="menu" :close-on-content-click="false" :nudge-width="200" offset-x</v-menu>
+   -->
+    <v-menu v-model="menu" :nudge-width="200" offset-x>
+      <!-- <template v-slot:activator="{ on, attrs }">
         <v-btn color="primary" dark v-bind="attrs" v-on="on">Menu as Popover</v-btn>
-      </template>
+      </template> -->
       <v-card dark>
         <v-list>
           <v-list-item>
-            <v-list-item-avatar>
-              <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
-            </v-list-item-avatar>
-
-            <v-list-item-content>
-              <v-list-item-title>John Leider</v-list-item-title>
-              <v-list-item-subtitle>Founder of Vuetify</v-list-item-subtitle>
-            </v-list-item-content>
-
+            <v-spacer></v-spacer>
             <v-list-item-action>
-              <v-btn :class="fav ? 'red--text' : ''" icon @click="fav = !fav">
-                <v-icon>mdi-heart</v-icon>
+              <v-btn @click="menu = false">
+                <v-icon>mdi-close-box</v-icon>
               </v-btn>
             </v-list-item-action>
           </v-list-item>
@@ -27,26 +22,17 @@
         <v-divider></v-divider>
 
         <v-list>
-          <v-list-item>
-            <v-list-item-action>
-              <v-switch v-model="message" color="purple"></v-switch>
-            </v-list-item-action>
-            <v-list-item-title>Enable messages</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-action>
-              <v-switch v-model="hints" color="purple"></v-switch>
-            </v-list-action>
-            <v-list-item-title>Enable Hints</v-list-item-title>
+          <v-list-item v-for="(item, index) in items" :key="index" @click="() => {
+          }">
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item>
         </v-list>
         <v-card-actions>
           <v-spacer></v-spacer>
         </v-card-actions>
 
-        <v-btn text @click="menu = false"> Cancel</v-btn>
-        <v-btn color="primary" text @click="menu = false">Save</v-btn>
+        <!-- <v-btn text @click="menu = false"> Cancel</v-btn>
+        <v-btn color="primary" text @click="menu = false">Save</v-btn> -->
       </v-card>
     </v-menu>
   </div>
@@ -68,6 +54,10 @@
 export default {
   name: "ModalMapComponent",
   data: () => ({
+    fav: true,
+    menu: true,
+    message: false,
+    hints: true,
     items: [
       {
         title: "Editar Desenho",
